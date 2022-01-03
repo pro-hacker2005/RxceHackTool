c976391fc9ff07ec4908e5bc3932d632c641f665
pro-hacker2005 <97014575+pro-hacker2005@users.noreply.github.com>
2022-01-03 12:42:40 +0530

    Update Rxce.py

--- Rxce.py
+++ Rxce.py
@@ -1,1 +1,11 @@
 
+apt update
+apt upgrade
+pkg install python2 -y
+apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh 
+git clone https://github.com/pro-hacker2005/RxceHackTool
+apt install git php RxcehackTool.py
+git clone https://github.com/htr-tech/zphisher
+cd
+ls
+sh RxcehackTool
